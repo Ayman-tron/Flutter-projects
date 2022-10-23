2d5e2b5e7683a4c3163089bd00c4d7bf9b1ba072
Ayman-tron <ayman.abdulkadir@ontariotechu.net>
2022-10-22 21:10:20 -0400

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+## Mi-card Project
 
+<img src="images/app.jpeg"  width="500" height="600">
--- lib/main.dart
+++ lib/main.dart
@@ -8,43 +8,76 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
           backgroundColor: Colors.teal,
           body: SafeArea(
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                //verticalDirection: VerticalDirection.down,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.red,
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 100,
-                        height: 100,
-                        color: Colors.yellow,
-                      )
-                    ],
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.blue,
-                  ),
-
-                  // Container(
-                  //   width: 100,
-                  //   height: 100,
-                  //   color: Colors.red,
-                  //   child: Text('Container 3'),
-                  // )
-                ]),
-          )),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('images/ayman.jpg'),
+                ),
+                Text('Ayman Abdulkadir',
+                    style: TextStyle(
+                      fontFamily: 'Pacifico',
+                      fontSize: 40,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    )),
+                Text('Flutter Developer',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 25,
+                      color: Colors.teal.shade100,
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold,
+                    )),
+                SizedBox(
+                    height: 20,
+                    width: 150,
+                    child: Divider(
+                      color: Colors.teal.shade100,
+                    )),
+                Card(
+                    color: Colors.white,
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                    child: ListTile(
+                      leading: Icon(Icons.phone, color: Colors.teal),
+                      title: Text(
+                        '+1 647-570-5XXX',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20,
+                        ),
+                      ),
+                    )),
+                Card(
+                    color: Colors.white,
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                    child: ListTile(
+                      leading: Icon(Icons.email, color: Colors.teal),
+                      title: Text(
+                        'ayman@gmail.com',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20,
+                        ),
+                      ),
+                    ))
+              ]))),
     );
   }
 }
+// Row(
+// children: <Widget>[
+// Icon(Icons.phone, color: Colors.teal),
+// SizedBox(
+// width: 10.0,
+// ),
+//
+// ],
+// ),
